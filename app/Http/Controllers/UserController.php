99431a001344c0bffb53ca7be9684329067e81db
Shady <shady.ber9@gmail.com>
2022-08-20 07:23:32 +0300

    oro language
    language added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,18 +1,32 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
-use App\Models\Role;
-use Illuminate\Database\QueryException;
-use Illuminate\Http\Request;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use PHPUnit\Exception;
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
-class UserController extends Controller
+class LoginController extends Controller
 {
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
@@ -21,131 +35,6 @@ class UserController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
-    }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-        return view('users.index')->with(['users'=>User::all(),'i'=>0]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-
-        return view ('users.create')->with(['roles'=>Role::all()]);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        if(Auth::user()->hasRole('admin')){
-            try {
-                $user= User::create([
-                    'name' => $request['name'],
-                    'email' => $request['email'],
-                    'password' => Hash::make($request['password']),
-                ]);
-                $user->roles()->attach(Role::find($request->input('role')));
-                $users=User::all();
-
-                return view('users.index')->with(['users'=>$users,'user'=>$user,'success'=>'User Created Succusfully']);
-
-            }catch (QueryException $ex){
-
-
-                return redirect()->back()->with('error','Query Error :'.$ex->errorInfo[2]);
-            }
-        }else{
-            return redirect()->back()->with('error','You Don\'t Have A Prermission');
-        }
-
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-        return view('users.show')->with('user',User::find($id));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-        return view ('users.edit')->with(['user'=>User::find($id),'roles'=>Role::all(),'userRole'=>User::find($id)->Role]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required',
-            'role' => 'required',
-
-        ]);
-
-        try{
-
-
-            \DB::table('users_roles')->insert([
-                'user_id' => $id,
-                'role_id' => $request->role,
-            ]);
-        }catch (QueryException $qx){
-
-        }
-        $user=User::find($id);
-        $user->update($request->all());
-
-
-        return redirect()->route('user.index')
-            ->with('success', 'User updated successfully');
-
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-        $user=User::find($id)->delete();
+        $this->middleware('guest')->except('logout');
     }
 }
--- config/languages.php
+++ config/languages.php
@@ -3,5 +3,6 @@
 return [
     'en' => 'English',
     'am' => 'Amharic',
+    'or' => 'Oromiffa',
 
 ];
